65838dbec265ca471169fc37624a1ecad6922053
Muhammad Zohaib Khan <itxlevicodez@gmail.com>
2025-06-15 19:56:21 +0500

    Update .gitignore and remove ignored files from tracking

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from agent_files.main_agent import main_agent
 from openai.types.responses import ResponseTextDeltaEvent
 import chainlit as cl
 
+
 @cl.on_chat_start
 async def chat_start():
     cl.user_session.set("history", [])
     await cl.Message(content="Hello there, how can i assist you today?").send()
+
 
 @cl.on_message
 async def main(message: cl.Message):
@@ -19,9 +21,11 @@ async def main(message: cl.Message):
         input=history,
     )
     async for event in response.stream_events():
-        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+        if event.type == "raw_response_event" and isinstance(
+            event.data, ResponseTextDeltaEvent
+        ):
             await msg.stream_token(event.data.delta)
     history.append({"role": "assistant", "content": response.final_output})
-    print(history,"\n")
+    # print(history, "\n")
     await msg.update()
     cl.user_session.set("history", history)
